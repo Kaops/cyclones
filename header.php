cb1f74df88680ca59798ef856a72692b81d4ec59
Kaops <stefan_polz@outlook.at>
2016-03-28 00:39:46 +0200

    updated home fixed logo

--- header.php
+++ header.php
@@ -1,21 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Cyclones Official</title>
-	<link rel="stylesheet" href="css/style.css">
-	<script type="text/javascript" src="js/jquery-1.12.1.min.js"></script>
-</head>
-<body>
-	<header class="site_header">
-		<h1 class="logo"><a href="index.php">Cyclones</a></h1>
-		<nav class="header_nav">
-			<ul>
-				<li <?php if ($site === "bio"): ?>class="header_nav_active"<?php endif; ?>><a href="index.php?site=bio" >Bio</a></li>
-				<li <?php if ($site === "music"): ?>class="header_nav_active"<?php endif; ?>><a href="index.php?site=music">Music</a></li>
-				<li <?php if ($site === "news"): ?>class="header_nav_active"<?php endif; ?>><a href="index.php?site=news">News</a></li>
-				<li <?php if ($site === "shop"): ?>class="header_nav_active"<?php endif; ?>><a href="index.php?site=shop">Shop</a></li>
-				<li><a href="#">Login</a></li>
-			</ul>
-		</nav>
-	</header>
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Cyclones Official</title>
+		<link rel="stylesheet" href="css/style.css">
+
+		<script type="text/javascript" src="js/jquery-1.12.1.min.js"></script>
+	</head>
+	<body>
+		<header class="site_header">
+			<div class="header_logo_wrapper">
+				<h1 class="logo"><a href="index.php">Cyclones</a></h1>
+			</div>
+			<nav class="header_nav">
+				<ul>
+					<li <?php if ($site === "bio"): ?>class="header_nav_active"<?php endif; ?>><a href="index.php?site=bio" >Bio</a></li>
+					<li <?php if ($site === "music"): ?>class="header_nav_active"<?php endif; ?>><a href="index.php?site=music">Music</a></li>
+					<li <?php if ($site === "news"): ?>class="header_nav_active"<?php endif; ?>><a href="index.php?site=news">News</a></li>
+					<li <?php if ($site === "shop"): ?>class="header_nav_active"<?php endif; ?>><a href="index.php?site=shop">Shop</a></li>
+					<li><a href="#">Login</a></li>
+				</ul>
+			</nav>
+		</header>
--- views/home.php
+++ views/home.php
@@ -1,7 +1,7 @@
-<main class="site_main_content" style="height: 925px">
-  <div class="site_main_sliderwrapper">
-  <div class="site_main_sliderleftbtn"><img src="img/slider_left.png" alt="" class="site_main_sliderleftimg"></div>
-    <div class="site_main_sliderrightbtn"><img src="img/slider_right.png" alt="" class="site_main_sliderrightimg"></div>
+<main class="site_main_content">
+  <div class="site_main_sliderwrapper" style="height: 885px">
+  <div class="site_main_sliderleftbtn"><a href="#" class="site_main_slidernavlink"><img src="img/slider_left.png" alt="" class="site_main_sliderleftimg"></a></div>
+    <div class="site_main_sliderrightbtn"><a href="#" class="site_main_slidernavlink"><img src="img/slider_right.png" alt="" class="site_main_sliderrightimg"></a></div>
     <div class="site_main_slidertext">
       <h2 class="site_main_sliderheader"> New Album </h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore officia possimus earum itaque voluptatibus, asperiores quod consequatur, repudiandae iure magni hic quo illum, temporibus provident ducimus fuga perspiciatis laudantium sequi.</p>
