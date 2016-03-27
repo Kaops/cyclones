4b51f0695a098e99fddd0d93775d04e1c71bf20a
Kaops <stefan_polz@outlook.at>
2016-03-28 01:35:53 +0200

    basic shop layout
    todo: cart buttons, onsale, stock display

--- views/shop.php
+++ views/shop.php
@@ -1,71 +1,93 @@
 <main class="site_main_content">
   <div class="site_main_shopwrapper">
-    <nav class="shop_nav">
-      <ul>
-        <li><a href="#">ALBUMS</a></li>
-        <li><a href="#">VINYL</a></li>
-        <li><a href="#">MERCH</a></li>
-        <li><a href="#">TICKETS</a></li>
-        <li><a href="#">SALE</a></li>
-      </ul>
-    </nav>
+    <div class="shop_nav_wrapper">
+      <nav class="shop_nav">
+        <ul>
+          <li><a href="#">ALBUMS</a></li>
+          <li><a href="#">VINYL</a></li>
+          <li><a href="#">MERCH</a></li>
+          <li><a href="#">TICKETS</a></li>
+          <li><a href="#">SALE</a></li>
+        </ul>
+      </nav>
+    </div>
     <div class="shop_nav_hr"></div>
 
-    <section class="shop_item">
-      <img src="shopitems/lost.jpg" alt="" />
-      <button class="shop_item_addtocart">Add to Cart</button>
-      <h3>LOST - ALBUM - VINYL EDITION</h3>
-      <small class="shop_item_pricetag">24,99 €</small>
-    </section>
+    <div class="shop_items">
 
-    <section class="shop_item">
-      <img src="shopitems/lost.jpg" alt="" />
-      <button class="shop_item_addtocart">Add to Cart</button>
-      <h3>LOST - ALBUM - VINYL EDITION</h3>
-      <small class="shop_item_pricetag">24,99 €</small>
-    </section>
+      <section class="shop_item">
+        <div class="shop_item_preview">
+          <img src="shopitems/lost.jpg" alt="" />
+          <button class="shop_item_addtocart">Add to Cart</button>
+        </div>
+        <h3>LOST - ALBUM - VINYL EDITION</h3>
+        <small class="shop_item_pricetag">24,99 €</small>
+      </section>
+      <section class="shop_item">
+        <div class="shop_item_preview">
+          <img src="shopitems/lost.jpg" alt="" />
+          <button class="shop_item_addtocart">Add to Cart</button>
+        </div>
+        <h3>LOST - ALBUM - VINYL EDITION</h3>
+        <small class="shop_item_pricetag">24,99 €</small>
+      </section>
 
-    <section class="shop_item">
-      <img src="shopitems/lost.jpg" alt="" />
-      <button class="shop_item_addtocart">Add to Cart</button>
-      <h3>LOST - ALBUM - VINYL EDITION</h3>
-      <small class="shop_item_pricetag">24,99 €</small>
-    </section>
+      <section class="shop_item">
+        <div class="shop_item_preview">
+          <img src="shopitems/lost.jpg" alt="" />
+          <button class="shop_item_addtocart">Add to Cart</button>
+        </div>
+        <h3>LOST - ALBUM - VINYL EDITION</h3>
+        <small class="shop_item_pricetag">24,99 €</small>
+      </section>
 
-    <section class="shop_item">
-      <img src="shopitems/lost.jpg" alt="" />
-      <button class="shop_item_addtocart">Add to Cart</button>
-      <h3>LOST - ALBUM - VINYL EDITION</h3>
-      <small class="shop_item_pricetag">24,99 €</small>
-    </section>
+      <section class="shop_item">
+        <div class="shop_item_preview">
+          <img src="shopitems/lost.jpg" alt="" />
+          <button class="shop_item_addtocart">Add to Cart</button>
+        </div>
+        <h3>LOST - ALBUM - VINYL EDITION</h3>
+        <small class="shop_item_pricetag">24,99 €</small>
+      </section>
 
-    <section class="shop_item">
-      <img src="shopitems/lost.jpg" alt="" />
-      <button class="shop_item_addtocart">Add to Cart</button>
-      <h3>LOST - ALBUM - VINYL EDITION</h3>
-      <small class="shop_item_pricetag">24,99 €</small>
-    </section>
+      <section class="shop_item">
+        <div class="shop_item_preview">
+          <img src="shopitems/lost.jpg" alt="" />
+          <button class="shop_item_addtocart">Add to Cart</button>
+        </div>
+        <h3>LOST - ALBUM - VINYL EDITION</h3>
+        <small class="shop_item_pricetag">24,99 €</small>
+      </section>
 
-    <section class="shop_item">
-      <img src="shopitems/lost.jpg" alt="" />
-      <button class="shop_item_addtocart">Add to Cart</button>
-      <h3>LOST - ALBUM - VINYL EDITION</h3>
-      <small class="shop_item_pricetag">24,99 €</small>
-    </section>
+      <section class="shop_item">
+        <div class="shop_item_preview">
+          <img src="shopitems/lost.jpg" alt="" />
+          <button class="shop_item_addtocart">Add to Cart</button>
+        </div>
+        <h3>LOST - ALBUM - VINYL EDITION</h3>
+        <small class="shop_item_pricetag">24,99 €</small>
+      </section>
 
-    <section class="shop_item">
-      <img src="shopitems/lost.jpg" alt="" />
-      <button class="shop_item_addtocart">Add to Cart</button>
-      <h3>LOST - ALBUM - VINYL EDITION</h3>
-      <small class="shop_item_pricetag">24,99 €</small>
-    </section>
+      <section class="shop_item">
+        <div class="shop_item_preview">
+          <img src="shopitems/lost.jpg" alt="" />
+          <button class="shop_item_addtocart">Add to Cart</button>
+        </div>
+        <h3>LOST - ALBUM - VINYL EDITION</h3>
+        <small class="shop_item_pricetag">24,99 €</small>
+      </section>
 
-    <section class="shop_item">
-      <img src="shopitems/lost.jpg" alt="" />
-      <button class="shop_item_addtocart">Add to Cart</button>
-      <h3>LOST - ALBUM - VINYL EDITION</h3>
-      <small class="shop_item_pricetag">24,99 €</small>
-    </section>
+      <section class="shop_item">
+        <div class="shop_item_preview">
+          <img src="shopitems/lost.jpg" alt="" />
+          <button class="shop_item_addtocart">Add to Cart</button>
+        </div>
+        <h3>LOST - ALBUM - VINYL EDITION</h3>
+        <small class="shop_item_pricetag">24,99 €</small>
+      </section>
+    </div>
+
+
 
   </div>
 </main>
