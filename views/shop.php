243ea5067ce6775b611bb9890adb0ec7e555c3b1
Kaops <stefan_polz@outlook.at>
2016-03-27 20:57:21 +0200

    re-adding files for home slider

---
+++ views/shop.php
@@ -0,0 +1,71 @@
+<main class="site_main_content">
+  <div class="site_main_shopwrapper">
+    <nav class="shop_nav">
+      <ul>
+        <li><a href="#">ALBUMS</a></li>
+        <li><a href="#">VINYL</a></li>
+        <li><a href="#">MERCH</a></li>
+        <li><a href="#">TICKETS</a></li>
+        <li><a href="#">SALE</a></li>
+      </ul>
+    </nav>
+    <div class="shop_nav_hr"></div>
+
+    <section class="shop_item">
+      <img src="shopitems/lost.jpg" alt="" />
+      <button class="shop_item_addtocart">Add to Cart</button>
+      <h3>LOST - ALBUM - VINYL EDITION</h3>
+      <small class="shop_item_pricetag">24,99 €</small>
+    </section>
+
+    <section class="shop_item">
+      <img src="shopitems/lost.jpg" alt="" />
+      <button class="shop_item_addtocart">Add to Cart</button>
+      <h3>LOST - ALBUM - VINYL EDITION</h3>
+      <small class="shop_item_pricetag">24,99 €</small>
+    </section>
+
+    <section class="shop_item">
+      <img src="shopitems/lost.jpg" alt="" />
+      <button class="shop_item_addtocart">Add to Cart</button>
+      <h3>LOST - ALBUM - VINYL EDITION</h3>
+      <small class="shop_item_pricetag">24,99 €</small>
+    </section>
+
+    <section class="shop_item">
+      <img src="shopitems/lost.jpg" alt="" />
+      <button class="shop_item_addtocart">Add to Cart</button>
+      <h3>LOST - ALBUM - VINYL EDITION</h3>
+      <small class="shop_item_pricetag">24,99 €</small>
+    </section>
+
+    <section class="shop_item">
+      <img src="shopitems/lost.jpg" alt="" />
+      <button class="shop_item_addtocart">Add to Cart</button>
+      <h3>LOST - ALBUM - VINYL EDITION</h3>
+      <small class="shop_item_pricetag">24,99 €</small>
+    </section>
+
+    <section class="shop_item">
+      <img src="shopitems/lost.jpg" alt="" />
+      <button class="shop_item_addtocart">Add to Cart</button>
+      <h3>LOST - ALBUM - VINYL EDITION</h3>
+      <small class="shop_item_pricetag">24,99 €</small>
+    </section>
+
+    <section class="shop_item">
+      <img src="shopitems/lost.jpg" alt="" />
+      <button class="shop_item_addtocart">Add to Cart</button>
+      <h3>LOST - ALBUM - VINYL EDITION</h3>
+      <small class="shop_item_pricetag">24,99 €</small>
+    </section>
+
+    <section class="shop_item">
+      <img src="shopitems/lost.jpg" alt="" />
+      <button class="shop_item_addtocart">Add to Cart</button>
+      <h3>LOST - ALBUM - VINYL EDITION</h3>
+      <small class="shop_item_pricetag">24,99 €</small>
+    </section>
+
+  </div>
+</main>
