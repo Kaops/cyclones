fcf210a85cf9a9c0a3b90cf87ca75e11baa02271
mwaltenberger <max.waltenberger@gmail.com>
2016-03-29 14:36:29 +0200

    checkout zwei schritte fertig

--- cyclonesofficial/js/jquery.js
+++ cyclonesofficial/js/jquery.js
@@ -2,3 +2,8 @@ $(window).load(function(){
 		var new_height = $(window).height() - 70;
 		$( ".site_main_sliderwrapper" ).css( "height", new_height );
 });
+$(document).ready(function () {
+    $(document).on("click", ".main_form_gender_btn", function () {
+        $(this).toggleClass("gender_btn--toggled");
+    });
+});
